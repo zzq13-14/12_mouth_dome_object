840b5639801444c1344f61c54165211d9c6d4f50
megetzz <53630338+megetzz@users.noreply.github.com>
2019-12-25 09:13:31 +0800

    Update music_sql.py

--- flask_2/music_sql.py
+++ flask_2/music_sql.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app=Flask(__name__)
 
 app.secret_key='qwertyuiop1'
-app.config['SQLALCHEMY_DATABASE_URI']='mysql+mysqlconnector://root:258000@localhost/music_project'
+app.config['SQLALCHEMY_DATABASE_URI']='mysql+mysqlconnector://root:******@localhost/music_project'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=True
 db=SQLAlchemy(app)
 
